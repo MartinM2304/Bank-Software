02e1e01193d0d5e30fd28cd36b4ad966820d6408
MartinM2304 <72139148+MartinM2304@users.noreply.github.com>
2023-01-01 01:51:14 +0200

    Add files via upload

--- functions.h
+++ functions.h
@@ -4,11 +4,21 @@
 #include <iostream>
 #include <string>
 int main();
-void login();
-void registration();
+void Menu(int lineIndex);
+void Login();
+void Registration();
+bool isUsernameFree(std::string username);
+void Deposit(int lineIndex);
+void Withdraw(int lineIndex);
+void Transfer(int lineIndex);
+void DeleteAccount(int lineIndex);
 void passwordHasher(std::string password);
 bool passwordChecker(std::string password);
 int passwordLength(std::string password);
-int Hasher(std::string &password);
+unsigned stringToInt(std::string str);
+double stringToDouble(std::string str);
+void overwriteFile(std::string name, std::string tempName,int lineIndex);
+void separateTheLine(std::string line, std::string &username,std::string &password,std::string &balance);
+std::string intToString(int n);
 
 #endif
